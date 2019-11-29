10cd71457b93b8a1aa74a7d15d38531373628bf7
Shrinath <Shrinathbhosale12@gmail.com>
2019-11-30 00:19:05 +0530

    factorial | recusion | C++

---
+++ recursion/factorial.cpp
@@ -0,0 +1,24 @@
+/*
+* factorial
+* Date: 30th Nov 2019
+* Author: Shrinath bhosale
+*/
+
+#include<iostream>
+
+using namespace std;
+
+long factorial(int n) {
+	if(n == 0)
+		return 1;
+
+	return n * factorial(n - 1);
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	cout << factorial(n) << endl;
+	return 0;
+}
